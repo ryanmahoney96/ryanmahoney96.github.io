a1dd9864bdd00e8ac1f119274b62bbedbf103c89
ryanmahoney96 <ryan.mahoney96@gmail.com>
2016-01-27 22:05:57 -0600

    test for ajax

--- Homework4/radius.js
+++ Homework4/radius.js
@@ -3,52 +3,72 @@
 var safe = 'https://www.zipcodeapi.com/rest/ZtNBtXVrOHxhei1EXjXTgwGPrEBZf8DfCqQ6oMxdt4uHnvmlPjf6QVVhzxhPGU4i/radius.json/';
 //<format>/<zip_code>/<distance>/<units>
 
+function foo(response) {
+	console.log(response);
+}
+
 function getRadius(zip, radius) {
 	
-	safe += zip + "/" + radius + "/mile";
+	safe = "https://www.zipcodeapi.com/rest/ZtNBtXVrOHxhei1EXjXTgwGPrEBZf8DfCqQ6oMxdt4uHnvmlPjf6QVVhzxhPGU4i/radius.json/" + zip + "/" + radius + "/mile";
+	// $.ajax({
+		// "url": safe,
+		// "dataType": "jsonp"
+	// }).done(function(data) {
+		// console.log("success");
+	// }).fail(function(data) {
+		// console.log("error");
+	// });
+	// var script = document.createElement('script');
+	// script.src = safe;
+	// document.body.appendChild(script);
 	
 	console.log(safe);
-	console.log(safe.zip_codes.length);
 	
-	$.ajax({
+	$.getJSON(safe, function(result){
+        console.log("success");
+    });
+
+	
+	// $.ajax({
 		
-		url: safe,
+		// url: safe,
 		
-		jsonp: "callback",
-		dataType: "jsonp",
+		// //jsonp: "callback",
+		// dataType: "jsonp",
 		
-		cache:false,
+		// //cache:false,
 		
-		success: function(response){
-			/*
-			console.log("success");
+		// success: function(response){
 			
-			var zipList = [];
+			// console.log("success");
+			
+			// var zipList = [];
 	
-			for(var i = 0; i < response.zip_codes.length; i++){
-				console.log("56");
-				zipList.push(response.zip_codes[i]);
-			}
-			*/
-			console.log("in succces block");
-		},
+			// for(var i = 0; i < response.zip_codes.length; i++){
+				// console.log("56");
+				// zipList.push(response.zip_codes[i]);
+			// }
+			
+			// console.log("in succces block");
+		// },
 		
-		error: function (response){
-			if(response.status == 200){
-				console.log("success");
-				console.log(response.length);
-				var zipList = [];
+		// error: function (response){
+			// if(response.status == 200){
+				// console.log("success");
+				// console.log(response.length);
+				// var zipList = [];
 	
-				for(var i = 0; i < response.zip_codes.length; i++){
-					console.log("56");
-					zipList.push(response.zip_codes[i]);
-				}
-			}
-			else {
-				console.log("Response: " + response);
-			}
-		}
-	});
+				// for(var i = 0; i < response.zip_codes.length; i++){
+					// console.log("56");
+					// zipList.push(response.zip_codes[i]);
+				// }
+			// }
+			// else {
+				// console.log("Response: " + response);
+			// }
+		// }
+		
+	// });
 }
 
 
